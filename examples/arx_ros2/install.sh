6c68847579b8dcbae6e6eb75eb20fa6de874fd70
duinodu <472365351duino@gmail.com>
2025-06-07 13:25:29 +0800

    move examples to picoxr_ros

---
+++ examples/arx_ros2/install.sh
@@ -0,0 +1,2 @@
+sudo apt install ros-foxy-rclcpp ros-foxy-tf2
+# sudo apt install ros-humble-rclcpp ros-humble-tf2
---
+++ examples/arx_ros2/urdf/README.md
@@ -0,0 +1,1 @@
+Mitigrated from https://github.com/ARXroboticsX/ARX_Model/tree/master/X7s
